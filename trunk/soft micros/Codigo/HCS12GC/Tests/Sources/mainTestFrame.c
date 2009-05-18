964ae0b0987b0e5da012f94185f724abbb24d7c9
jonymarino <jonymarino@9fc3b3b2-dce3-11dd-9b7c-b771bf68b549>
2009-05-18 15:33:38 +0000

    BaseTiempoDS1307Test

--- trunk/soft micros/Codigo/HCS12GC/Tests/Sources/mainTestFrame.c
+++ trunk/soft micros/Codigo/HCS12GC/Tests/Sources/mainTestFrame.c
@@ -1,4 +1,5 @@
 #include "IO_Map.h"
+#include "Cpu.h"
 #include "Object.h"
 #include "TestCase.h"
 #include "PE_Types.h"
@@ -15,7 +16,7 @@ struct ManejadorMemoria * pFlash = &flash;
 void main (void){
   
  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
- // PE_low_level_init();
+  PE_low_level_init();
  /*** End of Processor Expert internal initialization.                    ***/
   
   clrReg8Bits(COPCTL, 71); // kill the dog!
