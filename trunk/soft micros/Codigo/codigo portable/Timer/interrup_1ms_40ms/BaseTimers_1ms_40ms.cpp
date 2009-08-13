597cf186ef0006d38ef45e25a22fee08c0f5aace
nicopimen <nicopimen@9fc3b3b2-dce3-11dd-9b7c-b771bf68b549>
2009-08-13 18:44:57 +0000

    BaseTimer no se subio en el anterior

---
+++ trunk/soft micros/Codigo/codigo portable/Timer/interrup_1ms_40ms/BaseTimers_1ms_40ms.cpp
@@ -0,0 +1,43 @@
+#include "BaseTimers_1ms_40ms.hpp"
+#include "timer_interrupt.h"
+#include "TI1.h"
+
+
+BaseTimers_1ms_40ms* BaseTimers_1ms_40ms::instance=NULL;
+
+
+
+BaseTimers_1ms_40ms::BaseTimers_1ms_40ms(){
+  newAlloced(&on1ms,&Method,inc1,this);
+  newAlloced(&on40ms,&Method,inc40,this);
+  add1msListener(&on1ms);
+  add40msListener(&on40ms);
+}
+
+BaseTimers_1ms_40ms *BaseTimers_1ms_40ms::getInstance(void){
+  if(!instance)
+    instance = new BaseTimers_1ms_40ms();
+  return instance;
+}
+
+void BaseTimers_1ms_40ms::lockInc(){
+  TI1_Disable();
+}
+
+void BaseTimers_1ms_40ms::unlockInc(){
+  TI1_Enable();
+}
+
+void BaseTimers_1ms_40ms::inc1(void * _self){
+  BaseTimers_1ms_40ms * self=(BaseTimers_1ms_40ms *)_self;
+  self->incrementar(1);
+  self->actualizarTimers();
+}
+
+void BaseTimers_1ms_40ms::inc40(void * _self){
+  BaseTimers_1ms_40ms * self=(BaseTimers_1ms_40ms *)_self;
+  self->incrementar(TIEMPO_GRABACION);
+  self->actualizarTimers();
+}
+
+
