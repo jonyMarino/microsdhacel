6434fa07e97bf64561e2f64785077d843dbe3254
nicopimen <nicopimen@9fc3b3b2-dce3-11dd-9b7c-b771bf68b549>
2010-02-17 13:42:24 +0000

    main control

--- soft micros/Codigo/HC12_CPP/pruebas/aparatos/termometro/Sources/main.cpp
+++ soft micros/Codigo/HC12_CPP/pruebas/aparatos/termometro/Sources/main.cpp
@@ -22,8 +22,54 @@
 #include "Access.hpp"
 #include "DiagramaNavegacion.hpp"
 #include "VistaSetContrasenia.hpp"
+#include "VistaControl.hpp"
+#include "Salida.hpp"
+#include "IPWM.hpp"
+#include "PWM.hpp"
+#include "PWMHard.hpp"
+#include "PWMHard23.hpp"
+#include "PWMManager01_45.hpp"
+#include "configuracionControlPID.hpp"
+#include "SensorTermoPT100.hpp"
+#include "VistaPWM.hpp"
+#include "BoxPrincipalControl.hpp"
+#include "MessagesOut.hpp"
+
+void conectarSalidas(void * a);
+void OnTipoSalChange(void * b);
 
 #pragma CONST_SEG PARAMETERS_PAGE
+ 
+  /*volatile const Salida::confSalida salida_config={
+    0,            //potencia inicial
+    FALSE,        //inicia desconectada 
+    SALIDA_ONOFF  //tipo de salida inicial
+  };*/
+  
+/*volatile const TConfPWM confPWM01[CANTIDAD_SAL_ALARMA]={
+  0,
+  #if CANTIDAD_SAL_ALARMA>1 
+    0,
+      #if CANTIDAD_SAL_ALARMA>2
+        0,
+        #if CANTIDAD_SAL_ALARMA>3
+          0
+        #endif
+      #endif
+  #endif
+};
+         */
+/*  Tiempo inicial en el que el control permanece desconectado  */
+#ifdef _COLADA_CALIENTE  
+  #define SALIDA_TIEMPO_DESCONECTADA 20000 
+#else
+  #define SALIDA_TIEMPO_DESCONECTADA 3000   //tiene que alcanzar para hacer 2 mediciones
+#endif
+
+volatile const ConfiguracionControlPID::ControlConf control_config={
+   ControlDefaultConf,
+};
+  
   volatile const SensorTermoPT100::TConfSensor sensor_config[CANTIDAD_CANALES]= {
     STPT_DEF_CONF,
   #if CANTIDAD_CANALES>1 
@@ -42,6 +88,9 @@
  #else 
   volatile const int codigo = 0;
  #endif
+
+
+
 #pragma CONST_SEG DEFAULT
 
 
@@ -51,6 +100,7 @@ class Init{
   Init(){
     Timer::setBaseTimerDefault(*BaseTimers_1ms_40ms::getInstance());
     SensorTermoPT100::setConfiguracionTemperaturaAmbiente(&ta);
+
   }
 }ini;
 
@@ -68,10 +118,27 @@ SensorTermoPT100 sensor0(ad0,sensor_config[0],flash);
 SensorTermoPT100 sensor1(ad1,sensor_config[1],flash);
 #endif 
 
-const struct ConstructorBoxPrincipal cBoxPri={
-      &boxPrincipalFactory,							/* funcion que procesa al box*/
+TConfPWM confPWM01={
+0};
+PWMHard23 pwm(flash,confPWM01);
+const ConfiguracionControlPID configuraControl(*(ConfiguracionControlPID::ControlConf*)&control_config,flash); 
+ControlPID control0(sensor0,pwm,configuraControl);
+MessagesOut mjsCambioTipoSalida;
+
+//potencia
+const struct FstBoxPointer potInst={
+  (const struct ConstructorBox*)&cBoxPotInst,&control0,0  
+};
+
+const struct FstBoxPointer potMan={
+  (const struct ConstructorBox*)&cBoxPotMan,&control0,0  
+};
+
+const struct ConstructorBoxPrincipalControl cBoxPri={
+      &boxPrincipalControlFactory,							/* funcion que procesa al box*/
       &sensor0,      
-      NULL						
+      NULL,
+      &flash						
 };
 
 const struct FstBoxPointer principal={
@@ -79,7 +146,11 @@ const struct FstBoxPointer principal={
 };
 
 const struct FstBoxPointer *const opArray[]={
-  &principal  
+  &principal,
+  &potInst,
+  &potMan
+  
+  
 };
 
 /*const struct BoxList opList ={
@@ -90,7 +161,24 @@ const struct FstBoxPointer *const opArray[]={
 };*/
 const NEW_BOX_LIST(opList,opArray,"");
 
+//TUN        
 
+const struct FstBoxPointer reset={(const struct ConstructorBox*)&cBoxesReset,&control0,0};
+
+const struct FstBoxPointer aparatoConf={(const struct ConstructorBox*)&cBoxesSintonia,&control0,0};
+
+const struct FstBoxPointer periodo={(const struct ConstructorBox*)&cBoxPeriodo,&pwm,0};
+
+
+static const struct FstBoxPointer *const tunArray[]={
+  &reset,
+  &periodo,
+  &aparatoConf
+    
+};
+
+static const NEW_BOX_LIST(tun,tunArray,"SintoniA");
+ 
 //CAL
 const struct FstBoxPointer sensor1List={(const struct ConstructorBox*)&cBoxesSensor,&sensor0,1};
 
@@ -107,16 +195,37 @@ const VistaSetContrasenia vistaSetContrasenia={
 };
 const struct FstBoxPointer setCList={(const struct ConstructorBox*)&VistaSetContrasenia::cBoxSetContrasenia,(void*)&vistaSetContrasenia,0};
 
+const struct FstBoxPointer modosSalida={(const struct ConstructorBox*)&cBoxModoSalida,&control0,0};
+
 static const struct FstBoxPointer *const setArray[]={
-  &setCList  
+  &modosSalida,
+  &setCList
+    
 };
 
 static const NEW_BOX_LIST(set,setArray,"ConFigurAcion");
+
+//LIMITES        
+
+const struct FstBoxPointer limites={(const struct ConstructorBox*)&cBoxesLimites,&control0,0};
+
+static const struct FstBoxPointer *const limArray[]={
+  &limites,
+  
+    
+};
+
+static const NEW_BOX_LIST(lim,limArray,"LimitES");
+ 
+
+
   
 // Acceso comun
 const struct BoxList *const boxListArray[]={
+  &tun,
   &cal,
-  &set
+  &set,
+  &lim
 };
 
 const NEW_ACCESS(accesoComun,boxListArray,"Cod",(const int*)&codigo);
@@ -127,14 +236,29 @@ const struct Access *const accessArray[]={
 
 const NEW_ARRAY(accessList,accessArray);
 
- 
+void * timer=NULL; 
+
+struct Method timerSalida={
+&conectarSalidas,NULL
+}; 
+
+struct Method cambioTipoSalida={
+&OnTipoSalChange,NULL
+}; 
+
+//RlxMTimer timerConexionSalidas(SALIDA_TIEMPO_DESCONECTADA,timerSalida);
 
 void main(void) {
+  
+  BoxPrincipalControl::MostrarProp((ConstructorPropGetterVisual *)&cPropiedadSetPoint,&control0);
+  RlxMTimer timerConexionSalidas(SALIDA_TIEMPO_DESCONECTADA,timerSalida);
+  timer=&timerConexionSalidas;
   DiagramaNavegacion d(&opList,&accessList,FrenteDH::getInstancia());
   PE_low_level_init();
+  control0.addOnTipoSalidaListener(cambioTipoSalida);
   
   for(;;){
-  
+    
     byte tecla = FrenteDH::getInstancia()->getTecla();
     termometro.mainLoop();
     d.procesar(tecla);
@@ -151,4 +275,36 @@ void main(void) {
   
   /* please make sure that you never leave main */
 }
+ 
+ 
+void conectarSalidas(void * a){
+ byte i;
+  ((RlxMTimer *)timer)->stop();
   
+  //for(i=0;i<CANTIDAD_SAL_ALARMA;i++)
+   // pwm.setConectada(TRUE);
+  
+  // pwm.setConectada(TRUE);
+   
+   //configurar leds
+   //LedsSalida_init(&ledsSalida);
+   
+}
+
+
+
+void OnTipoSalChange(void * b){
+  
+  static MessagesOut::Message msj_on_sal_change;
+  
+  if(control0.getModoSalida()==ControlPID::_MAN){
+    if(!msj_on_sal_change)
+      msj_on_sal_change = mjsCambioTipoSalida.addMessage("MAn ");    
+  }else{
+    if(msj_on_sal_change){
+      mjsCambioTipoSalida.deleteMessage(msj_on_sal_change);
+      msj_on_sal_change=NULL;  
+    }                          
+  } 
+}
+ 
