7bbf8f5082d66d2f326d0adce45dbca40c265ca3
nicopimen <nicopimen@9fc3b3b2-dce3-11dd-9b7c-b771bf68b549>
2010-03-11 13:01:20 +0000

    limpiando svn

---
+++ soft micros/Codigo/HCS08AC/CODE/SelectorDigito.c
@@ -0,0 +1,8 @@
+#include "SelectorDigito.h"
+#include "portULN.h"
+
+
+void SelectorDigito_putVal(byte val){
+  portULN_PutVal(val);
+  
+}
---
+++ soft micros/Codigo/HCS08AC/CODE/SelectorDigito.h
@@ -0,0 +1,8 @@
+#ifndef _SELECTOR_DIGITO_H
+#define _SELECTOR_DIGITO_H
+
+#include "PE_Types.h"
+
+void SelectorDigito_putVal(byte val);
+
+#endif
