53e3987dc6a32ee5bbc083a39c8197391f6899dd
nicopimen <nicopimen@9fc3b3b2-dce3-11dd-9b7c-b771bf68b549>
2010-05-26 14:49:40 +0000

    sd(problemas con los 2 ultimos display y al reconocer las teclas)

--- soft micros/Codigo/HCS12GC/CODE/SelectorDigito.c
+++ soft micros/Codigo/HCS12GC/CODE/SelectorDigito.c
@@ -8,3 +8,12 @@ void SelectorDigito_putVal(byte val){
   bits5ULN_PutVal(val);
 }
 
+void SelectorDigitoInf_putVal(byte val){
+  bits2ULN_PutVal(0);
+  bits5ULN_PutVal(val);
+}
+
+void SelectorDigitoSup_putVal(byte val){
+  bits2ULN_PutVal(val);
+  bits5ULN_PutVal(0);
+}
--- soft micros/Codigo/HCS12GC/CODE/SelectorDigito.h
+++ soft micros/Codigo/HCS12GC/CODE/SelectorDigito.h
@@ -4,5 +4,7 @@
 #include "PE_Types.h"
 
 void SelectorDigito_putVal(byte val);
+void SelectorDigitoInf_putVal(byte val);
+void SelectorDigitoSup_putVal(byte val);
 
 #endif
